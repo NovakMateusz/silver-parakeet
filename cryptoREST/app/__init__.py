eb4844a2eab16cf8d76d5968b9dc611240a82e26
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2022-03-11 19:43:55 +0100

    Adding skeleton of REST API to communicate with external services

---
+++ cryptoREST/app/__init__.py
@@ -0,0 +1,21 @@
+from sanic import Sanic
+import json_logging
+
+from settings import load
+from app.utils.listeners import init_aiohttp_session
+
+
+def init_json_logging(app: Sanic):
+    json_logging.init_sanic(enable_json=True)
+    json_logging.init_request_instrument(app)
+
+
+
+def create_app() -> Sanic:
+    app_settings = load()
+    app = Sanic(app_settings.app_name)
+    from app.views import health_blueprint
+    app.blueprint(health_blueprint)
+    # app.register_listener(init_aiohttp_session, 'after_server_start')
+
+    return app
