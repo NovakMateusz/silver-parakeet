eb8e71def04f2490b06733d96c285413fc0b9e7d
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2022-03-13 18:21:36 +0100

    Renaming REST API

--- externalServicesREST/app/__init__.py
+++ externalServicesREST/app/__init__.py
@@ -10,12 +10,11 @@ def init_json_logging(app: Sanic):
     json_logging.init_request_instrument(app)
 
 
-
 def create_app() -> Sanic:
     app_settings = load()
     app = Sanic(app_settings.app_name)
     from app.views import health_blueprint
     app.blueprint(health_blueprint)
-    # app.register_listener(init_aiohttp_session, 'after_server_start')
+    app.register_listener(init_aiohttp_session, 'after_server_start')
 
     return app
