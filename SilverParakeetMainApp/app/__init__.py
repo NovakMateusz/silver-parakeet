210393ac15b14d966d1115604cc368180e4e7756
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2022-03-16 20:19:45 +0100

    Sending email with account activation link

--- SilverParakeetMainApp/app/__init__.py
+++ SilverParakeetMainApp/app/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 
-from .extensions import db, login_manager
 from settings import load
+from .extensions import db, login_manager, mail
 
 
 def register_blueprints(app: Flask):
-    from .user.views import user_blueprint
+    from .auth.views import auth_blueprint
     from .pages.views import pages_blueprint
     from .trading.views import trading_blueprint
     app.register_blueprint(pages_blueprint)
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(auth_blueprint)
     app.register_blueprint(trading_blueprint)
+
+
+def register_extensions(app: Flask):
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
 
 def create_app() -> Flask:
@@ -18,6 +24,6 @@ def create_app() -> Flask:
     register_blueprints(app)
     app_settings = load()
     app.config.from_object(app_settings)
-    db.init_app(app)
-    login_manager.init_app(app)
+    register_extensions(app)
+
     return app
--- SilverParakeetMainApp/app/auth/views/__init__.py
+++ SilverParakeetMainApp/app/auth/views/__init__.py
@@ -1,10 +1,22 @@
+import urllib.parse
+
 from flask import render_template, redirect, request, url_for
 from flask_login import login_required, logout_user, current_user, login_user
+from flask_mail import Message
 
-from app.user.forms import LoginForm, RegistrationForm
-from app.user import user_blueprint
-from app.user.models import User, AccountActivationLink
-from app.extensions import db, login_manager
+from app.auth.forms import LoginForm, RegistrationForm
+from app.auth import auth_blueprint
+from app.auth.models import User, AccountActivationLink
+from app.extensions import db, login_manager, mail
+
+
+def send_email(activation_key: str, recipient: str):
+    url_prefix = request.url_root
+    url_suffix = url_for('auth.account_activation_view', activation_id=activation_key)
+    activation_url = urllib.parse.urljoin(url_prefix, url_suffix)
+    msg = Message('Activate email', recipients=recipient)
+    msg.body = activation_url
+    mail.send(msg)
 
 
 @login_manager.user_loader
@@ -15,43 +27,44 @@ def load_user(user_id: int):
 
 @login_manager.unauthorized_handler
 def unauthorized():
-    return redirect(url_for('user.login_view'))
+    return redirect(url_for('auth.login_view'))
 
 
-@user_blueprint.route('/login', methods=['POST', 'GET'])
+@auth_blueprint.route('/login', methods=['POST', 'GET'])
 def login_view():
     if current_user.is_authenticated:
-        return redirect(url_for('home_blueprint.home_view'))
+        return redirect(url_for('pages.home_view'))
     login_form = LoginForm()
     if login_form.validate_on_submit():
         username = request.form.get('username')
         user = User.query.filter_by(username=username).first()
         if user and user.verify_password(request.form.get('password')):
             if not user.is_active():
-                return redirect(url_for('user.not_active_view'))
+                return redirect(url_for('auth.not_active_view'))
             login_user(user)
-            return redirect(url_for('home_blueprint.home_view'))
+            return redirect(url_for('pages.home_view'))
         else:
             print('No user')
     return render_template('login.html', form=login_form)
 
 
-@user_blueprint.route('/not-active')
+@auth_blueprint.route('/not-active')
 def not_active_view():
     return '<h1> Your account is not active </h1>'
 
 
-@user_blueprint.route('/logout')
+@auth_blueprint.route('/logout')
 @login_required
 def logout_view():
     logout_user()
-    return redirect(url_for('user.login_view'))
+    return redirect(url_for('auth.login_view'))
 
 
-@user_blueprint.route('/register', methods=['POST', 'GET'])
+@auth_blueprint.route('/register', methods=['POST', 'GET'])
 def register_view():
     if current_user.is_authenticated:
-        return redirect(url_for('home_blueprint.home_view'))
+        return redirect(url_for('pages.home_view'))
+
     registration_form = RegistrationForm()
     if registration_form.validate_on_submit():
         username = request.form.get('username')
@@ -59,16 +72,20 @@ def register_view():
         if not user:
             email = request.form.get('email')
             password = request.form.get('password')
+
             new_user = User(username=username, email=email, password=password)
-            db.session.add(new_user)
             new_link = AccountActivationLink(user=new_user)
+
+            db.session.add(new_user)
             db.session.add(new_link)
             db.session.commit()
-            return redirect(url_for('user.login_view'))
+
+            send_email(new_link.value, email)
+            return redirect(url_for('auth.login_view'))
     return render_template('registration.html', form=registration_form)
 
 
-@user_blueprint.route('/activate/<uuid:activation_id>')
+@auth_blueprint.route('/activate/<uuid:activation_id>')
 def account_activation_view(activation_id):
     activation_link = AccountActivationLink.query.filter_by(value=str(activation_id)).one_or_none()
     if activation_link and not activation_link.user.active:
