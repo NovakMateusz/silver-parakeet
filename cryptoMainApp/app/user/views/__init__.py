abc524f200a1ea650d5c1869fa41092d9956e323
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2022-04-18 22:40:01 +0200

    Adding front-end to contact view

--- cryptoMainApp/app/user/views/__init__.py
+++ cryptoMainApp/app/user/views/__init__.py
@@ -4,20 +4,7 @@ from flask import render_template
 from flask_login import login_required
 
 from app.user import user_blueprint
-from app.utils.constans import NAME_CODE_MAPPING
-
-
-def chunks(items: List, n: int = 5):
-    for i in range(0, len(items), n):
-        yield items[i:i + n]
-
-
-def prepare_items() -> List[List[Dict]]:
-    items: List[Dict] = [{'name': key, 'state': 0.0, 'filename': f'images/logos/{key.lower()}.svg'} for key in NAME_CODE_MAPPING.keys()]
-    result: List[List[Dict]] = []
-    for item in chunks(items):
-        result.append(item)
-    return result
+from app.user.utils import prepare_items
 
 
 @user_blueprint.route('/account')
