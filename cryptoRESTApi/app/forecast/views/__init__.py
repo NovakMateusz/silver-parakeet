2c957fc67770191bd3fff72f291850cec2c65f48
novak.mateusz94@gmail.com <novak.mateusz94@gmail.com>
2022-04-20 22:03:14 +0200

    Adding new endpoint to REST API - to get prediction for incoming day

--- cryptoRESTApi/app/forecast/views/__init__.py
+++ cryptoRESTApi/app/forecast/views/__init__.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+from typing import Dict
+
 from sanic import json, Request
+from pandas import Timestamp
 from pydantic import ValidationError
 
 from app.forecast import forecast_blueprint
 from app.forecast.models import (PredictionInputModel, PredictionErrorResponseModel, PredictionResponseModel,
-                                 CumulativePredictionResponseModel)
+                                 CumulativePredictionResponseModel, NextDayPredictionResponseModel)
 from app.utils.auth import protected
 from app.utils.constans import CODE_NAME_MAPPING
 from app.utils.errors import extract_message_from_error
@@ -33,4 +37,25 @@ async def predictions_view(request: Request):
     response_model = PredictionResponseModel(name=CODE_NAME_MAPPING[input_model.code],
                                              code=input_model.code,
                                              prediction=prediction.to_dict())
+    return json(response_model.dict())
+
+
+@forecast_blueprint.get('/next-day-prediction')
+@protected
+async def next_day_prediction_view(request: Request):
+    try:
+        input_model = PredictionInputModel(**request.args)
+    except ValidationError as error:
+        response_model = PredictionErrorResponseModel(endpoint=request.url,
+                                                      message=extract_message_from_error(error),
+                                                      status_code=400)
+        return json(response_model.dict(), status=response_model.status_code)
+    prediction: Dict = request.app.ctx.forecast_dictionary[input_model.code].to_dict()
+    today: datetime = datetime.now()
+    index: Timestamp = Timestamp(today.strftime("%Y-%m-%d 00:00:00"))
+    result: float = prediction['Close'][index]
+    response_model = NextDayPredictionResponseModel(name=CODE_NAME_MAPPING[input_model.code],
+                                                    code=input_model.code,
+                                                    prediction=result,
+                                                    date=today.strftime("%Y-%m-%d"))
     return json(response_model.dict())
